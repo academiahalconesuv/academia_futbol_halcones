7f76b891cab92fa6c02e8ca9ce29dad32ffe5dab
academiahalconesuv <academiahalconesuv@outlook.com>
2025-03-05 18:35:25 -0600

    Descomentado código de creación de usuario admin 4

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const reportesRoutes = require('./routes/reportes_routes');
 const app = express();
 const PORT = process.env.PORT || 3000; // HTTP
 const HTTPS_PORT = process.env.HTTPS_PORT || 3443; // HTTPS
-const adminRoutes = require('./routes/adminRoutes');
+const adminRoutes = require('./routes/admin_routestemp');
 app.use('/admin', adminRoutes);
 console.log("🔍 DATABASE_URL:", process.env.DATABASE_URL);
 
